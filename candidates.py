337911132aec67d8bf438314c3a4a6141c27426d
timosarkar <timosarkartimo@gmail.com>
2025-09-01 17:52:36 +0200

    tuned MAST filters

--- candidates.py
+++ candidates.py
@@ -1,15 +1,16 @@
 # WORKS: get a list of stars that might contain exoplanets transiting the star thats easy to detect
 from astroquery.mast import Catalogs
 
-# Query TIC for bright, small, cool stars
 tic_candidates = Catalogs.query_criteria(
-    catalog='TIC',
-    Tmag=[None, 12],
-    Teff=[None, 4000],
-    rad=[None, 0.7],
-    objType='STAR'
+    catalog="TIC",
+    objType="STAR",
+    Tmag=[9, 11],      # brightness
+    Teff=[2800, 3600], # temperature
+    rad=[None, 0.5],   # radius
 )
 
-# Print top 5 TIC IDs
-for row in tic_candidates[:5]:
-    print(row['ID'], row['Tmag'], row['Teff'], row['rad'])
+
+# around 1570 stars
+for row in tic_candidates:
+    print(row['ID'])
+
